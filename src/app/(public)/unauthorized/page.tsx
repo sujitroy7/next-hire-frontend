7cbbf91737363fd2c365425d2b2318c34b52bdbd
Sujit Roy <sujitroy78148@gmail.com>
2026-02-11 03:38:24 +0530

    relocate public file to public dir

---
+++ src/app/(public)/unauthorized/page.tsx
@@ -0,0 +1,10 @@
+export default function UnauthorizedPage() {
+  return (
+    <div className="min-h-screen text-center w-full">
+      <h1 className="text-3xl font-bold mt-44 block">403 – Unauthorized</h1>
+      <p className="mt-2 text-gray-500">
+        You don’t have permission to access this page.
+      </p>
+    </div>
+  );
+}

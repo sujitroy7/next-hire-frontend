07619e3a6bc1850ecbaaa0a419323a7a5f49956d
Sujit Roy <sujitroy78148@gmail.com>
2026-02-11 19:31:50 +0530

    Implement toaster

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { Providers } from "../store/Providers";
 import AuthInitializer from "@/components/shared/AuthInitializer";
+import { Toaster } from "sonner";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -29,6 +30,7 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+        <Toaster />
         <Providers>
           <AuthInitializer>{children}</AuthInitializer>
         </Providers>

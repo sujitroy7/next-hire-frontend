806bdeaa9343b47b79bc10e87b583a8c9d524ed8
Sujit Roy <sujitroy78148@gmail.com>
2026-02-18 20:47:27 +0530

    relocate address schema to _utils dir

--- src/app/org/profile/[id]/edit/_utils/schema.ts
+++ src/app/org/profile/[id]/edit/_utils/schema.ts
@@ -1,4 +1,5 @@
-import { addressSchema } from "@/components/shared/address-form-section/schema";
+import { addressSchema } from "@/schema/address";
+import { EmployeeCountEnum, OrganizationTypeEnum } from "@/types/organization";
 import * as z from "zod";
 
 export const formSchema = z.object({
@@ -8,24 +9,20 @@ export const formSchema = z.object({
   about: z.string().min(10, {
     message: "About description must be at least 10 characters.",
   }),
-  organizationType: z.string().min(1, {
-    message: "Please select an organization type.",
-  }),
-  employeeCount: z.string().min(1, {
-    message: "Please select an employee count range.",
-  }),
+  organizationType: z.nativeEnum(
+    OrganizationTypeEnum,
+    "Select valid organization type",
+  ),
+  employeeCount: z.nativeEnum(EmployeeCountEnum, "Select valid employee count"),
   websiteUrl: z
-    .string()
     .url({ message: "Please enter a valid URL." })
     .optional()
     .or(z.literal("")),
   linkedinUrl: z
-    .string()
     .url({ message: "Please enter a valid URL." })
     .optional()
     .or(z.literal("")),
   publicEmail: z
-    .string()
     .email({ message: "Please enter a valid email." })
     .optional()
     .or(z.literal("")),

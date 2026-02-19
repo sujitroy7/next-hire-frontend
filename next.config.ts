19543c94c4a80de558eba1f986cac537a4c7d3c3
Sujit Roy <sujitroy78148@gmail.com>
2026-02-19 19:30:38 +0530

    performance: add bundle analyzer to inspect NextJS bundle size

--- next.config.ts
+++ next.config.ts
@@ -13,4 +13,9 @@ const nextConfig: NextConfig = {
     removeConsole: process.env.NODE_ENV === "production",
   },
 };
-export default nextConfig;
+
+const withBundleAnalyzer = require("@next/bundle-analyzer")({
+  enabled: process.env.ANALYZE === "true",
+});
+
+export default withBundleAnalyzer(nextConfig);

70a80bb27c9c5bd7d10e0a74449b27e219acfac9
Sujit Roy <sujitroy78148@gmail.com>
2026-02-19 13:57:43 +0530

    optimize image resources

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,16 @@
+import { envRequired } from "@/lib/envRequired";
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   reactStrictMode: true,
+  images: {
+    remotePatterns: envRequired("WHITE_LISTED_IMAGE_WEBSITES")
+      .split(",")
+      .map((hostname) => ({ protocol: "https", hostname })),
+  },
+  reactCompiler: true,
+  compiler: {
+    removeConsole: process.env.NODE_ENV === "production",
+  },
 };
 export default nextConfig;
--- src/app/org/profile/[id]/_components/gallery.tsx
+++ src/app/org/profile/[id]/_components/gallery.tsx
@@ -1,4 +1,5 @@
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
+import Image from "next/image";
 
 interface Props {
   images: string[];
@@ -14,22 +15,16 @@ export default function OrgGallery({ images }: Props) {
       </CardHeader>
       <CardContent>
         <div className="grid grid-cols-1 sm:grid-cols-2 gap-4">
-          {images.map((src, index) => (
+          {images.map((src) => (
             <div
-              key={index}
+              key={src}
               className="relative aspect-video w-full overflow-hidden rounded-md border border-border/50"
             >
-              {/* TODO: Use Next.js Image component */}
-              {/* <Image
+              <Image
+                width={302}
+                height={169}
                 src={src}
-                alt={`Gallery Image ${index + 1}`}
-                fill
-                className="object-cover hover:scale-105 transition-transform duration-500"
-              /> */}
-              <img
-                src={src}
-                alt={`Gallery Image ${index + 1}`}
-                // fill
+                alt="Gallery Image"
                 className="object-cover hover:scale-105 transition-transform duration-500"
               />
             </div>

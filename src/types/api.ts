da108177ae199dfb159957897a36d9278462d957
Sujit Roy <sujitroy78148@gmail.com>
2026-02-20 17:52:54 +0530

    create reusable api types for response and params

--- src/types/api.ts
+++ src/types/api.ts
@@ -8,4 +8,21 @@ export type FailedResponse = {
   message: string;
 };
 
+export type Pagination = {
+  total: number;
+  page: number;
+  limit: number;
+  totalPages: number;
+};
+
+export type PaginationParams = {
+  page?: number;
+  limit?: number;
+};
+
+export type PaginationResponse<T> = {
+  data: T;
+  pagination: Pagination;
+};
+
 export type ApiResponse<T> = SuccessResponse<T> | FailedResponse;

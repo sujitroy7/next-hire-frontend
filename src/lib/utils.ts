d01cca3d0c199f185adce361e2f2d93c46154318
Sujit Roy <sujitroy78148@gmail.com>
2026-02-20 17:54:07 +0530

    create utility functions

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -8,3 +8,15 @@ export function cn(...inputs: ClassValue[]) {
 // for testing
 export const sleep = (ms: number) =>
   new Promise((resolve) => setTimeout(resolve, ms));
+
+export function getInitials(firstName: string, lastName: string): string {
+  return `${firstName.charAt(0)}${lastName.charAt(0)}`.toUpperCase();
+}
+
+export function formatDate(dateString: string): string {
+  return new Date(dateString).toLocaleDateString("en-US", {
+    month: "short",
+    day: "numeric",
+    year: "numeric",
+  });
+}

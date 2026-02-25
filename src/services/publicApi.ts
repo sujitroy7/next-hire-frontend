ac0b757085e42f458e3cbaea00763d7c84cb65ae
Sujit Roy <sujitroy78148@gmail.com>
2026-02-25 16:58:04 +0530

    write public api for fetching public jobs

--- src/services/publicApi.ts
+++ src/services/publicApi.ts
@@ -1,6 +1,13 @@
 import { AxiosInstance } from "axios";
 import { ApiResponse } from "@/types/api";
-import { Job } from "@/types/job";
+import { Job, PaginatedJobResponse } from "@/types/job";
+
+export async function getPublicJobs(
+  axios: AxiosInstance,
+  params?: Record<string, any>,
+) {
+  return axios.get<ApiResponse<PaginatedJobResponse>>("/jobs", { params });
+}
 
 export async function getJobById(axios: AxiosInstance, id: string) {
   return axios.get<ApiResponse<Job>>(`/jobs/${id}`);

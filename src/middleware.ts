1c389c19f10ce23bcccfedf8d785abec98aa9777
Sujit Roy <sujitroy78148@gmail.com>
2026-02-25 16:57:04 +0530

    whitelist pages

--- src/middleware.ts
+++ src/middleware.ts
@@ -12,6 +12,8 @@ const publicRoutes = [
   /^\/candidate\/profile\/[^/]+$/, // Matches /candidate/profile/123, but NOT /candidate/profile/123/edit
   /^\/org\/profile\/[^/]+$/, // Matches /org/profile/123, but NOT /org/profile/123/edit
   /^\/jobs\/view\/[^/]+$/, // Matches /jobs/view/123, but NOT /jobs/view/123/edit
+  /^\/jobs$/, // Matches /jobs
+  /^\/about$/, // Matches /about
 ];
 
 export async function middleware(request: NextRequest) {

98da048803d0c613cc1e0ffa2140967b9d379aa4
Sujit Roy <sujitroy78148@gmail.com>
2026-02-25 14:13:13 +0530

    Show layout on public routes if authenticated

---
+++ src/app/(public)/(with-layout)/layout.tsx
@@ -0,0 +1,25 @@
+import { getSession } from "@/lib/auth";
+import dynamic from "next/dynamic";
+import { Suspense } from "react";
+
+const OrgSidebar = dynamic(() => import("@/components/features/org-sidebar"));
+const RecruiterSidebar = dynamic(
+  () => import("@/components/features/recruiter-sidebar"),
+);
+
+export default async function PublicPageLayouts({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  const { userRole } = await getSession();
+  return (
+    <div className="flex h-screen overflow-hidden bg-background">
+      <Suspense>
+        {userRole === "ORGANIZATION" && <OrgSidebar />}
+        {userRole === "RECRUITER" && <RecruiterSidebar />}
+      </Suspense>
+      <main className="flex-1 overflow-y-auto">{children}</main>
+    </div>
+  );
+}

accae8f54268e45038af1c9225828dd5538bb87b
Sujit Roy <sujitroy78148@gmail.com>
2026-03-09 17:30:13 +0530

    auth: when auth failes redirect the user to login page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,6 +3,10 @@ import { redirectionPages } from "@/constants/redirects";
 import { redirect } from "next/navigation";
 
 export default async function Home() {
-  const { userRole } = await getSession();
-  redirect(redirectionPages[userRole]);
+  try {
+    const { userRole } = await getSession();
+    redirect(redirectionPages[userRole]);
+  } catch {
+    redirect("/login");
+  }
 }
